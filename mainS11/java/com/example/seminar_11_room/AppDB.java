b6af59499b166e1fa28934f607d1d4d139136c97
silviuser <silviuserban77@gmail.com>
2025-12-18 12:26:34 +0200

    test

--- mainS11/java/com/example/seminar_11_room/AppDB.java
+++
@@ -1,9 +0,0 @@
-package com.example.seminar_11_room;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Angajat.class}, version = 3)
-public abstract class AppDB extends RoomDatabase {
-    public abstract UserDAO userDao();
-}
